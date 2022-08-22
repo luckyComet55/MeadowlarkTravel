e8fee875a694b62bf13cfc6cf079f73bdb048e90
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2022-08-22 19:43:40 +0300

    A little step to the perfection

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ const {createServer} = require("http");
 const port = 3000;
 
 const server = createServer((req, res) => {
-    res.writeHead(200, {"ContentType": "text/plain"});
-    res.end("Hello, people!");
+    const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch(path) {
+        case "":
+            res.writeHead(200, {"ContentType": "text/plain"});
+            res.end("Homepage");
+            break;
+        case "/about":
+            res.writeHead(200, {"ContentType": "text/plain"});
+            res.end("I am Ivan, I like programming. This is not my hobby, yet I like it as a job, a profession");
+            break;
+        default:
+            res.writeHead(404, {"ContentType": "text/plain"});
+            res.end("Page does not exist");
+            break;
+    }
 })
 server.listen(port, "localhost", () => {
     console.log(`Server listening on port ${port}`)
