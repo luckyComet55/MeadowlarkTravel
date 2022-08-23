e0796ddc6b84dced7c354f9363e484eba4690a7d
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2022-08-23 11:27:19 +0300

    Added static express public path

--- meadowlark.js
+++ meadowlark.js
@@ -1,12 +1,17 @@
 import express from "express";
 import {engine} from "express-handlebars";
+import {URL} from "url";
 const port = 3000;
+const __dirname = new URL(".", import.meta.url).pathname;
 const app = express();
 
 app.engine("handlebars", engine({
     defaultLayout: "main"
 }))
 app.set("view engine", "handlebars");
+
+console.log(__dirname + "public")
+app.use(express.static("public"));
 
 app.get("/", (req, res) => res.render("home"));
 
