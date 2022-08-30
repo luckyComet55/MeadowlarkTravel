8cc4ac88b762604af1c0e3448238aeb292469d68
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2022-08-30 22:38:17 +0300

    Refactored for future testing

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,7 @@ import express from "express";
 import {engine} from "express-handlebars";
 import {URL} from "url";
 import getFortune from "./lib/fortune.js";
+import * as handlers from "./lib/handlers.js"
 const port = 3000;
 const __dirname = new URL(".", import.meta.url).pathname;
 const app = express();
@@ -16,23 +17,13 @@ app.set("view engine", "handlebars");
 console.log(__dirname + "public")
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", handlers.home);
 
-app.get("/about", (req, res) => {
-    const randomFortune = getFortune();
-    res.render("about", {fortune: randomFortune});
-});
+app.get("/about", handlers.about);
 
-app.use((req, res) => {
-    res.status(404);
-    res.render("404");
-})
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.status(500);
-    res.render("500");
-})
+app.use(handlers.serverError);
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
