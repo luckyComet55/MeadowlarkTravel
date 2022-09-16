90657ef1bfa30f724ae26742e2fc24ebf03398d7
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2022-09-16 22:02:03 +0300

    Added some fetch api

--- meadowlark.js
+++ meadowlark.js
@@ -1,26 +1,43 @@
 const express = require("express");
 const {engine} = require("express-handlebars");
 const handlers = require("./lib/handlers.js");
+const weatherMiddleware = require("./lib/middleware/weather");
+const body_parser = require("body-parser");
 const port = 3000;
 const app = express();
 
 
 
 app.engine("handlebars", engine({
-    defaultLayout: "main"
+    defaultLayout: "main",
+    helpers: {
+        section: function (name, options) {
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
 }))
 app.set("view engine", "handlebars");
 
-console.log(__dirname + "public")
 app.use(express.static("public"));
+app.use(weatherMiddleware);
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json());
 
 app.get("/", handlers.home);
 
 app.get("/about", handlers.about);
 
+app.get("/test", handlers.test);
+
+app.get("/newsletter", handlers.newsletter);
+app.post("/api/newsletter-signup", handlers.api.newsletterSignup);
+
 app.use(handlers.notFound);
 
 app.use(handlers.serverError);
+
 
 if(require.main === module) {
     app.listen(port, () => {
