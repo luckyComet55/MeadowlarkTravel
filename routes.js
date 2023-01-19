a352b480c940161d1b11be4aa20a6e2475d5e174
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-01-19 18:09:10 +0300

    Something

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const handlers = require("./lib/handlers");
-const api = require("./lib/handlers/api");
+const innerApi = require("./lib/handlers/innerApi");
 const uploadRel = require("./lib/handlers/uploadRelated");
 const newsletterRel = require("./lib/handlers/newsletterRelated");
 const main = require("./lib/handlers/main");
@@ -9,9 +9,9 @@ module.exports = app => {
     app.get("/about", main.about);
     app.get("/test", handlers.test);
     app.get("/newsletter", newsletterRel.newsletter);
-    app.post("/api/newsletter-signup", api.newsletterSignup);
+    app.post("/api/newsletter-signup", innerApi.newsletterSignup);
     app.get("/contest/vacation-photo/:year/:month", uploadRel.vacationPhotoContest);
-    app.post("/api/vacation-photo-contest/:year/:month", api.vacationPhotoContestApi);
+    app.post("/api/vacation-photo-contest/:year/:month", innerApi.vacationPhotoContestApi);
     app.get("/vacations", handlers.listVacations);
     app.get("/notify-when-in-season", handlers.notifyTrip);
     app.post("/trip-notify", handlers.notifyTripProcess);
